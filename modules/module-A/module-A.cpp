115700c01ea814dc6881e880926d9291166ebde7
andr0xfe <33175774+andr0xfe@users.noreply.github.com>
2018-01-13 14:24:37 +0800

    Update module-A.cpp

--- modules/module-A/module-A.cpp
+++ modules/module-A/module-A.cpp
@@ -3,3 +3,26 @@
 int main(){
     std::cout<<"hello,world!";
 }
+
+void  sayhello(){
+     std::cout<<"hello,world!";
+}
+
+static void static_sayhello(){
+      std::cout<<"hello,world!--static";
+}
+
+
+
+// Called when the library is loaded and before dlopen() returns
+static void __attribute__ ((constructor)) _load(void){
+    // Add initialization code…
+    std::cout<<"module wars loaded";
+}
+ 
+// Called when the library is unloaded and before dlclose()
+// returns
+static void  __attribute__ ((destructor)) _unload(void){
+    // Add clean-up code…
+    std::cout<<"module wanna unloaded";
+}
